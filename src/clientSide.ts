3cc4d5113cd0101cc4c09c78bd379268b3010e69
pasquale <pasfranc@gmail.com>
2021-04-14 12:23:37 +0200

    Initial commit

---
+++ src/clientSide.ts
@@ -0,0 +1,80 @@
+import { zencode_exec } from "zenroom";
+import { UserChallenges } from "./model/userChallenges";
+import {
+  fileExists,
+  readJSONFromFile,
+  readStringFromFile,
+} from "./service/fileService";
+import * as dotenv from "dotenv";
+
+const DEFAULT_CLIENT_SIDE_CONTRACT = "zencode/Keypair-Creation-Client-Side.zen";
+const REGULAR_EXPRESSION: RegExp = /\W/gi;
+const EMPTY_STRING: string = "";
+const DEFAULT_USER: string = "user";
+const DEFAULT_LOCALE = "en_GB";
+
+export const getSafetyQuestions = (userLocale: string) => {
+  dotenv.config();
+  const locale = userLocale ? userLocale : DEFAULT_LOCALE;
+  const propertiesFileName =
+    process.env.QUESTION_FOLDER! +
+    process.env.QUESTION_FILE_PREPEND! +
+    locale +
+    ".json";
+  const defaultPropertiesFileName = "props/questions-en_GB.json";
+  let questions: any;
+  if (fileExists(propertiesFileName)) {
+    questions = readJSONFromFile(propertiesFileName);
+  } else {
+    questions = readJSONFromFile(defaultPropertiesFileName);
+  }
+  return questions;
+};
+
+export const sanitizeAnswers = (answers: any) => {
+  for (const key in answers) {
+    if (answers[key]) {
+      answers[key] = answers[key]
+        .replace(REGULAR_EXPRESSION, EMPTY_STRING)
+        .toLowerCase();
+    }
+  }
+  return answers;
+};
+
+export async function recoveryKeypair(
+  answers: UserChallenges,
+  PBKDF: string,
+  username: string
+) {
+  const clientSideContract = process.env.CLIENT_SIDE_CONTRACT
+    ? process.env.CLIENT_SIDE_CONTRACT
+    : DEFAULT_CLIENT_SIDE_CONTRACT;
+  const CLIENT_SIDE_CONTRACT = readStringFromFile(clientSideContract);
+  const user = username ? username : DEFAULT_USER;
+
+  const keys: any = {
+    userChallenges: answers,
+    username: user,
+    key_derivation: PBKDF,
+  };
+  const data: any = {};
+
+  const execution: any = await zencode_exec(CLIENT_SIDE_CONTRACT, {
+    data: JSON.stringify(data),
+    keys: JSON.stringify(keys),
+  });
+
+  return JSON.parse(execution.result);
+}
+
+export async function verifyAnswers(
+  answers: UserChallenges,
+  PBKDF: string,
+  username: string,
+  userPublicKey: string
+) {
+  const execution = await recoveryKeypair(answers, PBKDF, username);
+
+  return userPublicKey === execution[username].keypair.public_key;
+}
---
+++ src/serverSide.ts
@@ -0,0 +1,31 @@
+import { zencode_exec } from "zenroom";
+import { readStringFromFile } from "./service/fileService";
+import * as dotenv from "dotenv";
+const DEFAULT_SERVER_SIDE_CONTRACT = "zencode/Keypair-Creation-Server-Side.zen";
+
+export async function createPBKDF(userData: any) {
+  dotenv.config();
+  const serverSideContract = process.env.SERVER_SIDE_CONTRACT
+    ? process.env.SERVER_SIDE_CONTRACT
+    : DEFAULT_SERVER_SIDE_CONTRACT;
+  const SERVER_SIDE_CONTRACT = readStringFromFile(serverSideContract);
+
+  const keys: any = {
+    theBackend: {
+      keypair: {
+        private_key: process.env.BACKEND_PRIVATE_KEY,
+        public_key: process.env.BACKEND_PUBLIC_KEY,
+      },
+    },
+    theBackendPassword: process.env.BACKEND_PASSWORD,
+    userData,
+  };
+  const data: any = {};
+
+  const execution: any = await zencode_exec(SERVER_SIDE_CONTRACT, {
+    data: JSON.stringify(data),
+    keys: JSON.stringify(keys),
+  });
+
+  return JSON.parse(execution.result);
+}
